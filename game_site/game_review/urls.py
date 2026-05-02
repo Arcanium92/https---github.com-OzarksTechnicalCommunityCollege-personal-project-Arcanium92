04703d653ed6b474a2e83952f4203423b3840d73
Nick <nm0960915@otc.edu>
2026-05-02 15:26:50 -0500

    Got the site structure working for the most part. need to work on smaller features like pokeapi and what not

--- game_site/game_review/urls.py
+++ game_site/game_review/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='home'),
+    path('', views.dashboard, name='dashboard'),
     path('add/', views.add_review, name='add_review'),
     path('reviews/', views.review_list, name='review_list'),
     path('reviews/<int:pk>/', views.review_result, name='review_result'),
--- game_site/game_site/urls.py
+++ game_site/game_site/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace='account')),
+    path('accounts/', include('django.contrib.auth.urls')),  # ← ADD THIS
     path('', include('game_review.urls')),
 ]
 
